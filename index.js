33228aba6018e639e090f85cc5a7d4eea25d209e
mj444555 <106246063+Mahi444555@users.noreply.github.com>
2023-01-30 11:15:01 +0530

    backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();  //calling express method
+const cors = require('cors')
+
+app.use(cors({
+    origin: "https://frontned-454545.onrender.com",
+}));
+
+app.use(express.json()) //if you want to use express with app then write like these
+const port = process.env.PORT || 5000
+
+const createConnection = require('./db')     //here we accessing that method from ./db.js page
+createConnection(); //calling method
+
+app.get("/", async(req,res)=>{
+    res.send("hello world!");
+})
+
+app.use('/api',require('./Routes/createUser'))      //accessing that router
+
+app.listen(port, ()=>{
+    console.log("server is running on port", port);
+})
